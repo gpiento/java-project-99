23fa9ec1fc5ff19ca49c21826ee479076f409a29
gpiento <zzzest@gmail.com>
2024-12-16 16:56:18 +0300

    step 10

---
+++ app/src/main/java/hexlet/code/config/OpenApiConfig.java
@@ -0,0 +1,15 @@
+package hexlet.code.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition
+@SecurityScheme(
+        name = "JWT",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class OpenApiConfig {
+}

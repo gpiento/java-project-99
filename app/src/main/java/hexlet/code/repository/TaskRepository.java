e566127cd5cf17f4fda7995fe11893ab7f3d8b2d
gpiento <zzzest@gmail.com>
2024-12-28 03:38:52 +0300

    Removes unused Modifying import.

--- app/src/main/java/hexlet/code/repository/TaskRepository.java
+++ app/src/main/java/hexlet/code/repository/TaskRepository.java
@@ -3,7 +3,6 @@ package hexlet.code.repository;
 import hexlet.code.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 

07417b1c2390e2de1d48c797f9f17f754d2df305
gpiento <zzzest@gmail.com>
2025-01-04 04:13:10 +0300

    Hardcodes default user credentials.

--- app/src/main/java/hexlet/code/component/DataInitializer.java
+++ app/src/main/java/hexlet/code/component/DataInitializer.java
@@ -36,10 +36,10 @@ public class DataInitializer implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
-        if (!userRepository.existsByEmail(defaultUserProperties.getEmail())) {
+        if (!userRepository.existsByEmail("hexlet@hexlet.com")) {
             User user = new User();
-            user.setEmail(defaultUserProperties.getEmail());
-            user.setPasswordDigest(defaultUserProperties.getPassword());
+            user.setEmail("hexlet@hexlet.com");
+            user.setPasswordDigest("qwerty");
             customUserDetailsService.createUser(user);
         }
 

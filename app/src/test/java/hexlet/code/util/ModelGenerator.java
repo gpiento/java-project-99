d1abfcafd678143dff6899b9608eba75a58ca25a
gpiento <zzzest@gmail.com>
2025-01-13 19:45:16 +0300

    Refactors model generation for consistent data.

--- app/src/test/java/hexlet/code/util/ModelGenerator.java
+++ app/src/test/java/hexlet/code/util/ModelGenerator.java
@@ -15,11 +15,15 @@ import hexlet.code.model.User;
 import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import net.datafaker.Faker;
+import net.datafaker.providers.base.Text;
 import org.instancio.Instancio;
 import org.instancio.Model;
 import org.openapitools.jackson.nullable.JsonNullable;
 import org.springframework.stereotype.Component;
 
+import static net.datafaker.providers.base.Text.DIGITS;
+import static net.datafaker.providers.base.Text.EN_LOWERCASE;
+import static net.datafaker.providers.base.Text.EN_UPPERCASE;
 import static org.instancio.Select.field;
 
 @Getter
@@ -50,6 +54,28 @@ public class ModelGenerator {
 
     private Model<LabelUpdateDTO> labelUpdateDTOModel;
 
+    private static String getName(Faker faker) {
+        return faker.lorem()
+                .sentence(3, 1);
+    }
+
+    private static String getSlug(Faker faker) {
+        return faker.lorem()
+                .sentence(2, 1)
+                .toLowerCase()
+                .replace(" ", "_")
+                .replaceAll("[^a-z_\\s]", "");
+    }
+
+    private static String getPassword(Faker faker) {
+        return faker.text().text(Text.TextSymbolsBuilder.builder()
+                .len(8)
+                .with(EN_LOWERCASE, 1)
+                .with(EN_UPPERCASE, 1)
+                .with(DIGITS, 1)
+                .build());
+    }
+
     @PostConstruct
     private void init() {
 
@@ -60,7 +86,7 @@ public class ModelGenerator {
                 .supply(field(User::getFirstName), () -> faker.name().firstName())
                 .supply(field(User::getLastName), () -> faker.name().lastName())
                 .supply(field(User::getEmail), () -> faker.internet().emailAddress())
-                .supply(field(User::getPasswordDigest), () -> faker.internet().password())
+                .supply(field(User::getPasswordDigest), () -> getPassword(faker))
                 .ignore(field(User::getCreatedAt))
                 .ignore(field(User::getUpdatedAt))
                 .toModel();
@@ -69,45 +95,31 @@ public class ModelGenerator {
                 .supply(field(UserCreateDTO::getEmail), () -> faker.internet().emailAddress())
                 .supply(field(UserCreateDTO::getFirstName), () -> faker.name().firstName())
                 .supply(field(UserCreateDTO::getLastName), () -> faker.name().lastName())
-                .supply(field(UserCreateDTO::getPassword), () -> faker.internet().password())
+                .supply(field(UserCreateDTO::getPassword), () -> getPassword(faker))
                 .toModel();
 
         userUpdateDTOModel = Instancio.of(UserUpdateDTO.class)
                 .supply(field(UserUpdateDTO::getEmail), () -> JsonNullable.of(faker.internet().emailAddress()))
                 .supply(field(UserUpdateDTO::getFirstName), () -> JsonNullable.of(faker.name().firstName()))
                 .supply(field(UserUpdateDTO::getLastName), () -> JsonNullable.of(faker.name().lastName()))
-                .supply(field(UserUpdateDTO::getPassword), () -> JsonNullable.of(faker.internet().password()))
+                .supply(field(UserUpdateDTO::getPassword), () -> JsonNullable.of(getPassword(faker)))
                 .toModel();
 
         taskStatusModel = Instancio.of(TaskStatus.class)
                 .ignore(field(TaskStatus::getId))
-                .supply(field(TaskStatus::getName), () -> faker.word().adverb().toLowerCase())
-                .supply(field(TaskStatus::getSlug), () -> faker.lorem()
-                        .sentence(2)
-                        .toLowerCase()
-                        .replace(" ", "_")
-                        .replaceAll("[^a-z_\\s]", ""))
+                .supply(field(TaskStatus::getName), () -> getName(faker))
+                .supply(field(TaskStatus::getSlug), () -> getSlug(faker))
                 .ignore(field(TaskStatus::getCreatedAt))
                 .toModel();
 
         taskStatusCreateDTOModel = Instancio.of(TaskStatusCreateDTO.class)
-                .supply(field(TaskStatusCreateDTO::getName), () -> faker.text()
-                        .text(3, 7, true))
-                .supply(field(TaskStatusCreateDTO::getSlug), () -> faker.lorem()
-                        .sentence(3)
-                        .toLowerCase()
-                        .replace(" ", "_")
-                        .replace("-", "_"))
+                .supply(field(TaskStatusCreateDTO::getName), () -> getName(faker))
+                .supply(field(TaskStatusCreateDTO::getSlug), () -> getSlug(faker))
                 .toModel();
 
         taskStatusUpdateDTOModel = Instancio.of(TaskStatusUpdateDTO.class)
-                .supply(field(TaskStatusUpdateDTO::getName), () -> JsonNullable.of(faker.text()
-                        .text(3, 7, true)))
-                .supply(field(TaskStatusUpdateDTO::getSlug), () -> JsonNullable.of(faker.lorem()
-                        .sentence(3)
-                        .toLowerCase()
-                        .replace(" ", "_")
-                        .replace("-", "_")))
+                .supply(field(TaskStatusUpdateDTO::getName), () -> JsonNullable.of(getName(faker)))
+                .supply(field(TaskStatusUpdateDTO::getSlug), () -> JsonNullable.of(getSlug(faker)))
                 .toModel();
 
         taskModel = Instancio.of(Task.class)
@@ -122,7 +134,7 @@ public class ModelGenerator {
                 .toModel();
 
         taskCreateDTOModel = Instancio.of(TaskCreateDTO.class)
-                .supply(field(TaskCreateDTO::getTitle), () -> faker.text().text(3, 7))
+                .supply(field(TaskCreateDTO::getTitle), () -> getName(faker))
                 .supply(field(TaskCreateDTO::getIndex), () -> JsonNullable.of(faker.number().numberBetween(1, 100)))
                 .supply(field(TaskCreateDTO::getContent), () -> faker.text().text(25))
                 .ignore(field(TaskCreateDTO::getStatus))
@@ -131,7 +143,7 @@ public class ModelGenerator {
                 .toModel();
 
         taskUpdateDTOModel = Instancio.of(TaskUpdateDTO.class)
-                .supply(field(TaskUpdateDTO::getTitle), () -> faker.text().text(3, 7))
+                .supply(field(TaskUpdateDTO::getTitle), () -> getName(faker))
                 .supply(field(TaskUpdateDTO::getIndex), () -> faker.number().numberBetween(1, 100))
                 .supply(field(TaskUpdateDTO::getContent), () -> faker.text().text(25))
                 .ignore(field(TaskUpdateDTO::getStatus))
@@ -141,25 +153,16 @@ public class ModelGenerator {
 
         labelModel = Instancio.of(Label.class)
                 .ignore(field(Label::getId))
-                .supply(field(Label::getName), () -> {
-                    int minLength = 3;
-                    int maxLength = 1000;
-                    int length = faker.random().nextInt(minLength, maxLength);
-                    return faker.lorem().fixedString(length).toLowerCase();
-                })
+                .supply(field(Label::getName), () -> getName(faker))
                 .ignore(field(Label::getCreatedAt))
                 .toModel();
 
         labelCreateDTOModel = Instancio.of(LabelCreateDTO.class)
-                .supply(field(LabelCreateDTO::getName), () -> faker.lorem()
-                        .sentence(1, 3)
-                        .toLowerCase())
+                .supply(field(LabelCreateDTO::getName), () -> getName(faker))
                 .toModel();
 
         labelUpdateDTOModel = Instancio.of(LabelUpdateDTO.class)
-                .supply(field(LabelUpdateDTO::getName), () -> JsonNullable.of(faker.lorem()
-                        .sentence(1, 3)
-                        .toLowerCase()))
+                .supply(field(LabelUpdateDTO::getName), () -> JsonNullable.of(getName(faker)))
                 .toModel();
     }
 }
